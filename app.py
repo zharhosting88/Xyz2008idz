a23bf7ab84ee7f1ee13d27f6530d220636a33cc5
zharhosting88 <zhar200830@gmail.com>
2025-10-16 22:10:35 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, jsonify
+from responses import RESPONSES
+import time
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("PyhtonBot.html")
+
+@app.route("/get_response", methods=["POST"])
+def get_response():
+    user_message = request.json.get("message", "").lower()
+    
+    # cari jawaban di dict RESPONSES
+    response = RESPONSES.get(user_message, "Maaf, aku belum paham maksudmu 😅")
+    
+    # simulasi delay seperti mengetik
+    time.sleep(1)
+    
+    return jsonify({"response": response})
+
+if __name__ == "__main__":
+    print(">> Flask app starting...")
+    app.run(debug=True)
---
+++ responses.py
@@ -0,0 +1,10 @@
+RESPONSES = {
+    "hai": "Hai juga! Apa kabar? 😊",
+    "apa kabar": "Aku baik, makasih sudah bertanya 😁",
+    "siapa namamu": "Aku Python 🐍",
+    "siapa kamu": "aku adalah ai yang di buat dengan menggunakan bahasa pyhton, developer ku bernama zhar"
+    "halo": "Halo juga! 👋",
+    "oh dasar lambat": "Heyy! Jaga bicaramu 😠",
+    "ooo hay si lambat": "Heyy! Jaga bicaramu 😠",
+    "bye": "Sampai jumpa 👋",
+}
